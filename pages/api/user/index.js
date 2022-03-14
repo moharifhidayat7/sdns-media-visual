3e1c1ee732adcbadea7cfe2f9c53837c16f5898d
Arif Hidayat <moharifhidayat7@outlook.com>
2022-03-14 21:38:30 +0700

    fix hash

--- pages/api/user/index.js
+++ pages/api/user/index.js
@@ -18,22 +18,27 @@ export default async function handler(req, res) {
     try {
       const { password } = req.body;
       const saltRounds = 10;
-      bcrypt.hash(password, saltRounds, async function (err, hash) {
-        const user = await prisma.user.create({
-          data: {
-            ...req.body,
-            password: hash,
-          },
-        });
+      const hash = bcrypt.hash(
+        password,
+        saltRounds,
+        async function (err, hash) {
+          return hash;
+        }
+      );
+      const user = await prisma.user.create({
+        data: {
+          ...req.body,
+          password: hash,
+        },
+      });
 
-        delete user.password;
-        delete user.resetPasswordToken;
+      delete user.password;
+      delete user.resetPasswordToken;
 
-        res.statusCode = 200;
-        res.json({
-          message: "User created",
-          user,
-        });
+      res.statusCode = 200;
+      res.json({
+        message: "User created",
+        user,
       });
     } catch (error) {
       res.status(403).json({ err: "Error occured." });
