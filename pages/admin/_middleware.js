7c742b7251fac06c5d4894f08c47c4297fcd3ac9
Arif Hidayat <moharifhidayat7@outlook.com>
2022-03-22 16:28:07 +0700

    middleware

--- pages/admin/_middleware.js
+++ pages/admin/_middleware.js
@@ -3,13 +3,12 @@ import { getToken } from "next-auth/jwt";
 
 export async function middleware(req) {
   const secret = process.env.NEXTAUTH_SECRET;
+  const url = process.env.NEXTAUTH_URL;
   const token = await getToken({ req, secret });
 
   if (token) {
     return NextResponse.next();
   } else {
-    return NextResponse.redirect(
-      process.env.NEXTAUTH_URL + "/login?r=" + req.url
-    );
+    return NextResponse.redirect(url + "/login?r=" + req.url);
   }
 }
