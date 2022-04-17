861ce030152756c8d4283a678d5881c88be586bf
Arif Hidayat <moharifhidayat7@outlook.com>
2022-04-17 09:45:30 +0700

    proteksi halaman

--- pages/admin/_middleware.js
+++ pages/admin/_middleware.js
@@ -6,6 +6,25 @@ export async function middleware(req) {
   const token = await getToken({ req });
 
   if (token) {
+    const akses = token.user.role.akses.map((aks) => {
+      if (aks.read == false && aks.write == false) {
+        return {
+          ...aks,
+          visible: false,
+        };
+      }
+      return {
+        ...aks,
+        visible: true,
+      };
+    });
+
+    const page = akses.filter((f) => url + f.path == req.url);
+
+    if (page.length == 0 || page[0].visible == false) {
+      return NextResponse.redirect(url + "/403");
+    }
+
     return NextResponse.next();
   } else {
     return NextResponse.redirect(url + "/login?callbackUrl=" + req.url);
