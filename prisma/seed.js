bb9870246f3a5cfbbace08a157095a1a919033c8
Arif Hidayat <moharifhidayat7@outlook.com>
2022-04-18 07:41:53 +0700

    setup seeder

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,5 +1,5 @@
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
+const prisma = require("../lib/prisma");
+
 async function main() {
   const administrator = await prisma.role.upsert({
     where: { nama: "ADMINISTRATOR" },
