1aedf31cb29134afa1a209261b0c83f653920e12
achrizal15 <achrizal15@gmail.com>
2022-04-27 09:07:42 +0700

    Update form.jsx

--- pages/admin/pelanggan/form.jsx
+++ pages/admin/pelanggan/form.jsx
@@ -62,7 +62,7 @@ export async function getServerSideProps(context) {
     const data = await res.json();
     return data.kecamatan;
   });
-  const id_kecamatan = result.kecamatan.split("-")[0];
+  const id_kecamatan = result.kecamatan[0].split("-")[0];
   const kelurahan = await fetch(`https://dev.farizdotid.com/api/daerahindonesia/kelurahan?id_kecamatan=${id_kecamatan}`).then(res => res.json());
 
   return {
